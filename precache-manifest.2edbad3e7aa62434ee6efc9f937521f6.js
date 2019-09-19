7e4a005f2b3aa0ed2f0b9e520ca8c08229d987be
Uzair Hussain <k163791@nu.edu.pk>
2019-09-19 15:18:27 +0500

    Updates

---
+++ precache-manifest.2edbad3e7aa62434ee6efc9f937521f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8e3ce9d76e2805318ef8f80bddbf50",
+    "url": "/bike-rental-app/index.html"
+  },
+  {
+    "revision": "242b5f2e030b8e6bba08",
+    "url": "/bike-rental-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c44099ed7ab72d319bfc",
+    "url": "/bike-rental-app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "242b5f2e030b8e6bba08",
+    "url": "/bike-rental-app/static/js/2.1e83f467.chunk.js"
+  },
+  {
+    "revision": "c44099ed7ab72d319bfc",
+    "url": "/bike-rental-app/static/js/main.69fce781.chunk.js"
+  },
+  {
+    "revision": "8f6c072ff7284f617c2a",
+    "url": "/bike-rental-app/static/js/runtime~main.936fb8fd.js"
+  }
+]);
